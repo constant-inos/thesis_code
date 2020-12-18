a6e8e022fe158122191e2d22f56abd15c9fb7d79
constant-inos <48479891+constant-inos@users.noreply.github.com>
2020-12-18 16:59:37 +0200

    Update DDQN.py

--- controllers/ppo_webots/DDQN.py
+++ controllers/ppo_webots/DDQN.py
@@ -6,15 +6,15 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 import os
 from networks import DQNetwork
+from experience_memory import *
 
 class Agent(object):
 
-    def __init__(self, state_size, action_size, lr=0.0001, batch_size=32, \
+    def __init__(self, action_size, lr=0.0001, batch_size=32, \
                  gamma=0.99, epsilon_max=1.0, epsilon_min=0.0001,\
                  update_target_freq=3000, train_interval=100, \
                  mem_size=50000, fname='dqn.h5'):
         
-        self.state_size = state_size
         self.action_size = action_size
         self.action_space = [i for i in range(action_size)]
         self.lr = lr
@@ -27,8 +27,7 @@ class Agent(object):
         self.train_interval = train_interval
         self.model_file = fname
 
-        self.memory = deque(maxlen=mem_size)
-        self.mem_cntr = 0
+        self.memory = Memory(n_actions=action_size)
 
         self.model = DQNetwork(action_size)
         self.model.compile(loss='mse',optimizer=Adam(lr))
@@ -42,41 +41,21 @@ class Agent(object):
             action = self.model(state).numpy()[0]
             action_idx = np.argmax(action)
         return action_idx
-
-    def sample_memory(self,n_samples):
-        samples = random.sample(self.memory,n_samples)
-        
-        states = np.zeros((n_samples,)+self.state_size)
-        action_ind = np.zeros((n_samples,))
-        rewards = np.zeros((n_samples,))
-        new_states = np.zeros((n_samples,)+self.state_size)
-        dones = np.zeros((n_samples,))
-
-        for i in range(n_samples):
-            states[i] = samples[i][0]
-            action_ind[i] = samples[i][1]
-            rewards[i] = samples[i][2]
-            new_states[i] = samples[i][3]
-            dones[i] = samples[i][4]
-
-        return states,action_ind,rewards,new_states,dones
     
     def store_experience(self,state,action,reward,new_state,done):
-        self.memory.append((state,action,reward,new_state,1-int(done)))
-        self.mem_cntr += 1
+        self.memory.store_experience(state,action,reward,new_state,1-int(done))
 
     def learn(self):
-
         if self.epsilon > self.epsilon_min:
             self.epsilon -= (self.epsilon_max - self.epsilon_min) / 50000
-        if self.mem_cntr % self.update_target_freq == 0:
+        if self.memory.memCounter % self.update_target_freq == 0:
             self.update_target_model()
 
-        if not (agent.mem_cntr % agent.train_interval == 0):
+        if not (self.memory.memCounter % self.train_interval == 0):
             return
 
-        n_samples = min(self.batch_size*self.train_interval, self.mem_cntr)
-        states,action_ind,rewards,new_states,notdones = self.sample_memory(n_samples)
+        n_samples = min(self.batch_size*self.train_interval, self.memory.memCounter)
+        states,action_ind,rewards,new_states,notdones = self.memory.sample_memory(n_samples)
 
         q_pred = self.model.predict(states)
         q_eval = self.model.predict(new_states)
@@ -100,7 +79,7 @@ if __name__ == '__main__':
     import gym
 
     env = gym.make('CartPole-v0')
-    agent = Agent(state_size=(4,),action_size=2)
+    agent = Agent(action_size=2)
 
     n_games = 2000
     scores = []
