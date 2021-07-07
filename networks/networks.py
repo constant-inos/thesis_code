98a2a234bafa023ff24ecaaf26b29eb394cadbb8
constant-inos <48479891+constant-inos@users.noreply.github.com>
2021-07-07 14:27:26 +0300

    Update networks.py

--- networks/networks.py
+++ networks/networks.py
@@ -1,189 +1,200 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense,Conv2D,Flatten,Concatenate
+from tensorflow.keras.layers import Dense,Conv2D,Flatten,Concatenate,MaxPooling2D
 
-class PPONetwork(keras.Model):
-    def __init__(self,n_actions,conv=False):
-        super(PPONetworkConv,self).__init__()
 
-        self.HiddenLayers = []
+class DenseNet(keras.Model):
+    def __init__(self,units=[64]):
+        super(DenseNet, self).__init__()
 
-        if conv:
-            self.HiddenLayers.append( Conv2D(32,kernel_size=8,strides=(4,4),activation='relu') )
-            self.HiddenLayers.append( Conv2D(64,kernel_size=4,strides=(2,2),activation='relu') )
-            self.HiddenLayers.append( Conv2D(64,kernel_size=3,activation='relu') )
-            self.HiddenLayers.append( Flatten() )
+        self.Layers = []
+        for n in units:
+            self.Layers.append( Dense(units=n, activation='relu') )
 
-        self.HiddenLayers.append( Dense(256,activation='relu') )
-        self.HiddenLayers.append( Dense(256,activation='relu') )
+    def call(self,INPUT):
+        x = INPUT
+
+        for layer in self.Layers:
+            x = layer(x)
+            
+        return x
         
-        self.v = Dense(1,activation='linear')
+        
+class ConvNet(keras.Model):
+    def __init__(self,filters=[64,64]):
+        super(ConvNet, self).__init__()
+
+        self.Layers = []
+        for n in filters:
+            self.Layers.append( Conv2D(filters=n, kernel_size=3, activation='relu') )
+            self.Layers.append( MaxPooling2D((2,2)) )
+        self.Layers.append( Flatten() )
+
+    def call(self,INPUT):
+        x = INPUT
+
+        for layer in self.Layers:
+            x = layer(x)
+            
+        return x
+        
+        
+class SimpleDQN(keras.Model):
+    def __init__(self,output_size):
+        super(SimpleDQN,self).__init__()
+        
+        self.main = DenseNet(units=[128,128,128])
+        self.out = Dense(output_size,activation='linear')
+        
+    def call(self,INPUT):
+        x = INPUT
+        x = self.main(x)
+        x = self.out(x)
+        return x
+        
+class ConvDQN(keras.Model):
+    def __init__(self,output_size):
+        super(ConvDQN,self).__init__()
+        
+        self.conv = ConvNet(filters=[64,64])
+        self.main = DenseNet(units=[128,128])
+        self.out = Dense(output_size,activation='linear')
+        
+    def call(self,INPUT):
+        x = INPUT
+
+        x = self.conv(x)
+        x = self.main(x)
+        x = self.out(x)
+        
+        return x
+        
+class ComplexDQN(keras.Model):
+    def __init__(self,output_size):
+        super(ComplexDQN,self).__init__()
+        
+        self.conv = ConvNet(filters=[64,64])
+        self.simple = DenseNet(units=[64])
+        self.concat = Concatenate(axis=-1)
+        
+        self.main = DenseNet(units=[128,128])
+        self.out = Dense(output_size,activation='linear')
+        
+    def call(self,INPUT):
+        x1 = INPUT[0] # camera data
+        x2 = INPUT[1] # sensors data
+        
+        x1 = self.conv(x1)
+        x2 = self.simple(x2)
+        x = self.concat([x1,x2])
+        x = self.main(x)
+        x = self.out(x)
+        
+        return x
+        
+        
+class SimplePGNet(keras.Model):
+    def __init__(self,n_actions):
+        super(SimplePGNet, self).__init__()
+        self.n_actions = n_actions
+
+        self.main = DenseNet(units=[24,48])
         self.pi = Dense(n_actions,activation='softmax')
 
     def call(self,state):
         x = state
+        x = self.main(x)
+        pi = self.pi(x)
 
-        for layer in self.HiddenLayers:
-            x = layer(x)
+        return pi
 
-        policy = self.pi(x)
-        value = self.v(x)
-
-        return policy, value
-
-
-# class PPONetwork(keras.Model):
-#     def __init__(self,n_actions):
-#         super(PPONetwork,self).__init__()
-
-#         self.fc1 = Dense(256,activation='relu')
-#         self.fc2 = Dense(256,activation='relu')
-        
-#         self.v = Dense(1,activation='linear')
-#         self.pi = Dense(n_actions,activation='softmax')
-
-#     def call(self,state):
-#         x = self.fc1(state)
-#         x = self.fc2(x)
-
-#         policy = self.pi(x)
-#         value = self.v(x)
-
-#         return policy, value
-
-class ActorCriticNetwork(keras.Model):
+class SimpleACNet(keras.Model):
     def __init__(self, n_actions, name='actor_critic'):
-        super(ActorCriticNetwork, self).__init__()
+        super(SimpleACNet, self).__init__()
         self.n_actions = n_actions
         self.model_name = name
 
-        self.layer1 = Dense(1024, activation='relu')
-        self.layer2 = Dense(512, activation='relu')
+        self.main = DenseNet([48,24])
         self.v = Dense(1, activation='linear')
         self.pi = Dense(n_actions,activation='softmax')
 
     def call(self,state):
-        value = self.layer1(state)
-        value = self.layer2(value)
-
-        pi = self.pi(value)
-        v = self.v(value)
+        x = state
+        x = self.main(x)
+        pi = self.pi(x)
+        v = self.v(x)
         
         return v,pi
 
-class PolicyGradientNetwork(keras.Model):
-    def __init__(self,n_actions):
-        super(PolicyGradientNetwork, self).__init__()
+class Actor(keras.Model):
+    def __init__(self, n_actions, name='actor'):
+        super(Actor, self).__init__()
         self.n_actions = n_actions
+        self.model_name = name
 
-        self.fc1 = Dense(256,activation='relu')
-        self.fc2 = Dense(256,activation='relu')
+        self.main = DenseNet([48,24])
         self.pi = Dense(n_actions,activation='softmax')
 
     def call(self,state):
-        value = self.fc1(state)
-        value = self.fc2(value)
-
-        pi = self.pi(value)
+        x = state
+        x = self.main(x)
+        pi = self.pi(x)
 
         return pi
+    
+class Critic(keras.Model):
+    def __init__(self, n_actions, name='critic'):
+        super(Critic, self).__init__()
+        self.n_actions = n_actions
+        self.model_name = name
 
-
-class DQNetwork(keras.Model):
-    def __init__(self,action_size,conv=False):
-        super(DQNetwork, self).__init__()
-        self.HiddenLayers = []
-
-        if conv:
-            self.HiddenLayers.append( Conv2D(32,kernel_size=8,strides=(4,4),activation='relu') )
-            self.HiddenLayers.append( Conv2D(64,kernel_size=4,strides=(2,2),activation='relu') )
-            self.HiddenLayers.append( Conv2D(64,kernel_size=3,activation='relu') )
-            self.HiddenLayers.append( Flatten() )
-        self.HiddenLayers.append( Dense(units=512, activation='relu') )
-
-        self.value = Dense(units=action_size, activation='linear')
+        self.main = DenseNet([48,24])
+        self.v = Dense(1, activation='linear')
 
     def call(self,state):
         x = state
-
-        for layer in self.HiddenLayers:
-            x = layer(x)
-
-        value = self.value(x)
-
-        return value
-
-class MitsosPPONet(keras.Model):
-    def __init__(self,n_actions):
-        super(MitsosPPONet, self).__init__()
-        self.ConvLayers = []
-        self.ConvLayers.append( Conv2D(64,kernel_size=9,activation='relu') )
-        self.ConvLayers.append( Conv2D(64,kernel_size=5,activation='relu') )
-        self.ConvLayers.append( Conv2D(64,kernel_size=3,activation='relu') )
-        
-        self.flatten = Flatten() 
-        self.concat = Concatenate(axis=-1)
-        
-        self.DenseLayers = []
-        self.DenseLayers.append( Dense(512,activation='relu') )
-        self.DenseLayers.append( Dense(256,activation='relu') )
-
-        self.policy = Dense(n_actions,activation='softmax')
-        self.value = Dense(1,activation='linear')
-    
-    def call(self,state):
-        x1 = state[0] #stacked frames
-        x2 = state[1] #stacked sensor values
-        
-        for layer in self.ConvLayers:
-            x1 = layer(x1)
-            
-        x1 = self.flatten(x1)
-        x2 = self.flatten(x2)
-        x = self.concat([x1,x2])
-        
-        for layer in self.DenseLayers:
-            x = layer(x)
-        
-        pi = self.policy(x)
-        v = self.value(x)
-        
-        return pi,v
-        
-
-class MitsosDQNet(keras.Model):
-    def __init__(self,action_size):
-        super(MitsosDQNet, self).__init__()
-        self.ConvLayers = []
-        self.ConvLayers.append( Conv2D(64,kernel_size=9,activation='relu') )
-        self.ConvLayers.append( Conv2D(64,kernel_size=5,activation='relu') )
-        #self.ConvLayers.append( Conv2D(64,kernel_size=3,activation='relu') )
-        
-        self.flatten = Flatten() 
-        self.concat = Concatenate(axis=-1)
-        
-        self.DenseLayers = []
-        self.DenseLayers.append( Dense(units=512, activation='relu') )
-        self.DenseLayers.append( Dense(units=512, activation='relu') )
-        self.DenseLayers.append( Dense(units=512, activation='relu') )
-
-        self.value = Dense(units=action_size, activation='linear')
-    
-    def call(self,state):
-        x1 = state[0] #stacked frames
-        x2 = state[1] #stacked sensor values
-        
-        for layer in self.ConvLayers:
-            x1 = layer(x1)
-            
-        x1 = self.flatten(x1)
-        x2 = self.flatten(x2)
-        x = self.concat([x1,x2])
-        
-        for layer in self.DenseLayers:
-            x = layer(x)
-        
-        v = self.value(x)
+        x = self.main(x)
+        v = self.v(x)
         
         return v
+
+
+class SimpleDDPG_actor(keras.Model):
+    def __init__(self,n_actions,name='ddpg_actor'):
+        super(SimpleDDPG_actor, self).__init__()
+        self.n_actions = n_actions
+        self.model_name=name 
+        last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
+
+        # ACTOR
+
+        self.actor_main = DenseNet([256,256])
+        self.actor_out = Dense(n_actions,activation='tanh',kernel_initializer=last_init)
+
+
+    def call(self,state):
+        x = self.actor_main(state)
+        action = self.actor_out(x)  # UPPER BOUND?
+        return action 
+
+class SimpleDDPG_critic(keras.Model):
+    def __init__(self,n_actions,name='ddpg_critic'):
+        super(SimpleDDPG_critic,self).__init__()
+        self.n_actions = n_actions
+        self.model_name=name 
+
+        self.critic_state_in = DenseNet([16,32])
+        self.critic_action_in = DenseNet([32])
+
+        self.critic_main = DenseNet([256,256])
+        self.critic_out = Dense(n_actions)
+
+    def call(self,state,action):
+        x_a = self.critic_state_in(state)
+        x_b = self.critic_action_in(action)
+        x = Concatenate(axis=-1)([x_a,x_b])
+        x = self.critic_main(x)
+        value = self.critic_out(x)
+        return value
+
 
