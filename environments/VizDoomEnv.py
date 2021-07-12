7dbf0e41dc3386dd0bd6f8227022b165bd90d69f
constant-inos <48479891+constant-inos@users.noreply.github.com>
2021-07-12 10:44:38 +0300

    Update VizDoomEnv.py

--- environments/VizDoomEnv.py
+++ environments/VizDoomEnv.py
@@ -10,6 +10,7 @@ from skimage.viewer import ImageViewer
 
 import numpy as np
 from collections import deque
+import cv2
 
 
 def preprocessImg(img, size):
@@ -22,7 +23,7 @@ def preprocessImg(img, size):
 
 
 class VizDoomEnv(object):
-    def __init__(self,state_size=(64,64,4),scenario='defend_the_center.cfg'):
+    def __init__(self,state_size=(64,64,4),scenario='defend_the_center.cfg',record_episode=False):
         game = DoomGame()
         path_to_scenario = os.path.join(current_dir,scenario)
         game.load_config(path_to_scenario)
@@ -43,10 +44,14 @@ class VizDoomEnv(object):
         self.life = deque(maxlen=30)
         self.kills = deque(maxlen=30)
 
+        self.record_episode = record_episode
+        self.game_rec = []
+        
     def reset(self):
         self.game.new_episode()
         game_state = self.game.get_state()
         frame = game_state.screen_buffer # initial resolution 480 x 640
+        if self.record_episode: self.game_rec.append(frame)
         frame = preprocessImg(frame, size=(self.state_size[0], self.state_size[1])) # 64x64
         state = np.stack(([frame]*4),axis=2) # 64x64x4 (stack the same frame)
         #self.state = np.expand_dims(state,axis=0) #1x64x64x4
@@ -54,7 +59,6 @@ class VizDoomEnv(object):
         self.prev_misc = game_state.game_variables
         #print('new episode')
         return self.state
-
 
     def step(self,action_idx):
         # perform action
@@ -72,10 +76,16 @@ class VizDoomEnv(object):
             #print('LIFE:',self.steps,'KILLS:',self.kills[-1],'AVG-KILLS:',np.mean(self.kills))
             self.steps = 0
             self.game.new_episode()
+            
+            if len(self.game_rec)>0: 
+                self.store_game_rec()
+                self.game_rec = []
+                self.record_episode = False
         game_state = self.game.get_state()
         reward = self.game.get_last_reward()
 
         new_frame = game_state.screen_buffer
+        if self.record_episode: self.game_rec.append(new_frame)
         misc = game_state.game_variables
 
         (img_rows, img_cols) = (self.state_size[0], self.state_size[1])
@@ -101,3 +111,32 @@ class VizDoomEnv(object):
             r_t = r_t - 0.1
 
         return r_t
+
+
+    def store_game_rec(self):
+        
+        filename = 'vizdoom_rec_0'
+        filenames = os.listdir(current_dir) # returns list
+        while(True):
+            if filename in filenames:
+                filename = filename[:-1] + str(int(filename[-1])+1)
+            else:
+                break
+            
+        path = os.path.join(current_dir,filename)
+        keep_variables = np.array(self.game_rec,dtype=object)
+        f = open(path,'wb')
+        np.save(f,keep_variables)
+        f.close()
+
+        # img_array = self.game_rec
+        # image = (img_array[0]*255).astype(np.uint8)
+        # size = image.shape[:2]
+        # out = cv2.VideoWriter(filename+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, frameSize=(size[1],size[0]) )
+         
+        # for i in range(len(img_array)):
+        #     image = img_array[i]
+        #     image = (image*255).astype(np.uint8)
+        #     out.write(image)
+        # out.release()
+        
